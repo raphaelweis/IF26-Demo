2eb3998e695074cb5b028088ba14a896f8d584f2
Raphaël Weis <raphael.weis@outlook.com>
2024-12-11 17:27:19 +0100

    doc: Added a small README section for the demo.

--- README.md
+++ README.md
@@ -1,8 +1,27 @@
-# Welcome to your Expo app 👋
+# Demo IF26.
+
+Pour tester le projet :
+```
+# Installer les dépendances
+npm i
+
+# Lancer le serveur de développement
+npm start
+```
+
+Une fois le serveur de développement lancé, scanner le QR code dans le terminal
+avec Expo Go ou bien ouvrir un émulateur android et appuyer sur 'a'.
+
+J'ai rajouté un composant custom qui peut être utilisé pour l'explication de code.
+Il se trouve dans [`/components/DemoIF26.tsx`](/components/DemoIF26.tsx).
+
+En dessous de cette section le README auto généré par Expo :
+
+## Welcome to your Expo app 👋
 
 This is an [Expo](https://expo.dev) project created with [`create-expo-app`](https://www.npmjs.com/package/create-expo-app).
 
-## Get started
+### Get started
 
 1. Install dependencies
 
@@ -25,7 +44,7 @@ In the output, you'll find options to open the app in a
 
 You can start developing by editing the files inside the **app** directory. This project uses [file-based routing](https://docs.expo.dev/router/introduction).
 
-## Get a fresh project
+### Get a fresh project
 
 When you're ready, run:
 
@@ -35,14 +54,14 @@ npm run reset-project
 
 This command will move the starter code to the **app-example** directory and create a blank **app** directory where you can start developing.
 
-## Learn more
+### Learn more
 
 To learn more about developing your project with Expo, look at the following resources:
 
 - [Expo documentation](https://docs.expo.dev/): Learn fundamentals, or go into advanced topics with our [guides](https://docs.expo.dev/guides).
 - [Learn Expo tutorial](https://docs.expo.dev/tutorial/introduction/): Follow a step-by-step tutorial where you'll create a project that runs on Android, iOS, and the web.
 
-## Join the community
+### Join the community
 
 Join our community of developers creating universal apps.
 
--- components/DemoIF26.tsx
+++ components/DemoIF26.tsx
@@ -1,12 +1,19 @@
+import React from "react";
 import { useEffect, useState } from "react";
 import { StyleSheet, View, Text, Button } from "react-native";
 
 // Ceci est un composant custom pour la démo react-native de l'UE IF26.
 export default function DemoIF26() {
-  // react-native recommande TypeScript pour développer de nouveaux projets.
+  // react-native recommande TypeScript pour développer de nouveaux projets. Ce
+  // fichier est donc un fichier TSX au lieu de JSX, et on peut spécifier le type
+  // de notre hook useState.
   const [count, setCount] = useState<number>(0);
 
-  // En react-native, on déclare le style directement dans les fichiers source:
+  // En react-native, on déclare le style directement dans les fichiers source. Il
+  // peut être déclaré à l'intérieur ou à l'extérieur des composants, selon ce qui
+  // est le plus pertinent. Les StyleSheet react-native sont très proches du CSS
+  // sur le Web, bien qu'elles n'offrent pas toutes les propriétés que le CSS moderne
+  // possède.
   const demoIF26Style = StyleSheet.create({
     text: {
       color: "white",
@@ -23,7 +30,10 @@ export default function DemoIF26() {
 
   // C'est un composant react standard, on peut y utiliser tous les hooks fournis
   // par react sur le web.
-  useEffect(() => console.log("Hello, IF26!"), []);
+  useEffect(
+    () => console.log(`Hello, IF26! Le compte d'étudiants est: ${count}`),
+    [count]
+  );
 
   // Comme en react standard, on retourne du JSX. La seule différence est que nous
   // n'avons pas accès aux composants HTML standards présents sur le Web.
@@ -33,7 +43,7 @@ export default function DemoIF26() {
     <>
       <View style={{ height: 16 }} />
       <Text style={demoIF26Style.text}>
-        Nombre d'élèves aujourd'hui en IF26:
+        Nombre d'élèves en IF26:
       </Text>
       <Text style={[demoIF26Style.text, demoIF26Style.counterText]}>
         {count}
